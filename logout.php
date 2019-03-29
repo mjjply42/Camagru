abbb1f3c4c8a7c2b337a7fff893442e4eda646f8
Matthew <mjjply@Matthews-Air.attlocal.net>
2019-03-29 02:21:50 -0700

    fixed deletions

--- logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$_SESSION['logged'] == false;
-session_write_close();
-
-?>
--- usr_session.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-$_SESSION['logged'] = true;
-session_write_close();
-header("Location: usr_profile.php");
-?>
